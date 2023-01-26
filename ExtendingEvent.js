e647fc1ef0a556da6be9f215260b33dbc989035e
yakkantiChakradhar <120570592+yakkantiChakradhar@users.noreply.github.com>
2023-01-26 12:16:10 +0530

    Add files via upload

---
+++ ExtendingEvent.js
@@ -0,0 +1,17 @@
+const EventEmitter = require("node:events");
+
+class PizzaShop extends EventEmitter{
+    constructor(){
+        super();
+        this.orderNumber = 0;
+    }
+    order(size,topping){
+        this.orderNumber++;
+        this.emit("order",size,topping);
+    }
+    displayOrderNumber(){
+        console.log(`current order number:${this.orderNumber}`);
+    }
+}
+
+module.exports = PizzaShop;
