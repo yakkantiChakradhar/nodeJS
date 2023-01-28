e7a9df104d7bfcc79062d5a09a327ebf6e2c14b8
yakkantiChakradhar <120570592+yakkantiChakradhar@users.noreply.github.com>
2023-01-28 11:34:56 +0530

    Update fspipes.js

--- fspipes.js
+++ fspipes.js
@@ -6,6 +6,7 @@ const rStream = fsstreams.createReadStream("./fstext.txt",{
     encoding: "utf-8", highWaterMark:64,
 });
 rStream.pipe(gzip).pipe(fsstreams.WriteStream("./fstxt3.txt.gz"))
+
 const wStrean = fsstreams.createWriteStream("./fstxt3.txt");
 
 rStream.pipe(wStrean);
